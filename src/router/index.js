324688476c69b72bd6f967cea14bbd3aff367796
Steve <stevegojkov@gmail.com>
2019-06-26 08:46:34 -0400

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Crypto from '@/components/Crypto'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Crypto',
+      component: Crypto
     }
   ]
 })
